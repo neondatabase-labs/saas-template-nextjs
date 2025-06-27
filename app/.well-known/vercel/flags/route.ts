d9ec6f0f8cd7376eb7524f5bd9fa323d62b7fab5
Andre Landgraf <andre.timo.landgraf@gmail.com>
2025-06-27 09:37:53 -0700

    fix

--- app/.well-known/vercel/flags/route.ts
+++ app/.well-known/vercel/flags/route.ts
@@ -1,11 +1,13 @@
 import { verifyAccess, type ApiData } from "flags"
 import { getProviderData } from "flags/next"
 import { NextResponse, type NextRequest } from "next/server"
-import { plansFlag } from "../../../api/stripe/plans"
+import { getPlansFlag } from "../../../../lib/stripe/plans"
 
 export async function GET(request: NextRequest) {
 	const access = await verifyAccess(request.headers.get("Authorization"))
 	if (!access) return NextResponse.json(null, { status: 401 })
+
+	const plansFlag = await getPlansFlag()
 
 	const providerData = getProviderData({
 		plans: plansFlag,
